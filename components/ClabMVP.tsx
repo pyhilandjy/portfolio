fa1d5ad001c1fa91fcc7bcc94d1a543fdcc361d8
pyhilandjy <hilandjy@gmail.com>
2024-11-18 21:33:45 +0900

    chore: 데이터 후처리 추가

--- components/ClabMVP.tsx
+++ components/ClabMVP.tsx
@@ -41,7 +41,7 @@ const ClabMVP: React.FC = () => {
             </li>
             <li className="flex items-start">
               <span className="font-bold w-20 mb-3">프로세스</span>
-              <span className="block ml-2 mb-3">
+              <span className="block ml-2 mb-6">
                 1. 사용자로부터 오디오 파일을 수집
                 <br />
                 2. STT 이후 오탈자, 발화자 정보 수정
@@ -53,7 +53,7 @@ const ClabMVP: React.FC = () => {
             </li>
             <li className="flex items-start">
               <span className="font-bold w-20 mb-3">주요 작업</span>
-              <span className="block ml-2 mb-3">
+              <span className="block ml-2 mb-6">
                 - Database Modeling
                 <br />
                 - 데이터 전처리 및 데이터 가공 API 및 워크스페이스 구축
@@ -65,14 +65,20 @@ const ClabMVP: React.FC = () => {
             <li className="flex flex-col items-start">
               <span className="font-bold w-20 mb-4">작업상세</span>
               <span className="block ml-2 mb-3">
-                <span className="block font-bold">1. 데이터베이스 모델링</span>
+                <span className="block font-bold mb-1">
+                  1. 데이터베이스 모델링
+                </span>
                 <span className="block ml-4">
-                  ∙ 프로젝트 초기 단계에서 빈번한 스키마 변경이 예상되어, 데이터
-                  일관성과 중복 최소화를 위한 정규화 원칙을
+                  ∙ 데이터 일관성과 중복 최소화를 위한{" "}
+                  <strong className="font-bold">정규화 원칙을 적용</strong>
+                  하되, 프로젝트 초기 단계에서{" "}
+                  <strong className="font-bold">빈번한 스키마 변경이</strong>
                 </span>
                 <span className="block ml-8 mb-2">
-                  적용하되 유연한 구조 확보를 위해 외래키 제약조건은 제외하여
-                  설계를 진행했습니다.
+                  <strong className="font-bold">예상</strong>되어, 유연한 구조
+                  확보를 위해{" "}
+                  <strong className="font-bold">외래키 제약조건은 제외</strong>
+                  하여 설계를 진행했습니다.
                 </span>
                 <span className="block ml-4">
                   ∙ 정규화를 적용한 결과, 테이블 간 관계가 복잡해지고 참조해야
@@ -82,41 +88,43 @@ const ClabMVP: React.FC = () => {
                   어려움을 겪었습니다. 이로인해 API 설계 시 로직을 메모 하는
                   습관이 생겼고, 이후 API 설계 및 구현에 있어서
                 </span>
-                <span className="block ml-8">
+                <span className="block ml-8 mb-4">
                   더 빠르게 진행할 수 있었습니다.
                 </span>
                 <span className="block ml-8"></span>
               </span>
               <span className="block ml-2 mb-3">
-                <span className="block font-bold">
+                <span className="block font-bold mb-1">
                   2. REST API 아키텍처 설계
                 </span>
                 <span className="block ml-4">
-                  ∙ 외래키가 없는 테이블 구조에서 데이터 무결성 관리에 중점을
-                  두어 설계했으며, 특히 상위 테이블의 데이터 삭제 시
+                  ∙ 외래키가 없는 테이블 구조에서{" "}
+                  <strong className="font-bold">
+                    데이터 무결성 관리에 중점을 두어 설계
+                  </strong>
+                  했으며, 특히 상위 테이블의 데이터 삭제 시
                 </span>
                 <span className="block ml-8 mb-2">
                   발생할 수 있는 고아 데이터 문제를 해결하기 위해 고민했습니다.
                 </span>
                 <span className="block ml-4">
-                  ∙ 하위 데이터가 존재하는 경우 삭제 시도를 차단하는 보호 장치를
-                  구현하고, 사용자에게 하위 데이터의 존재를 알리고
+                  ∙ 하위 데이터가 존재하는 경우 사용자에게 하위 데이터의 존재를
+                  알리고, 전체 삭제 여부를 확인하는 알림을 추가하여
                 </span>
-                <span className="block ml-8">
-                  전체 삭제 여부를 확인하는 알림 시스템을 도입하여 데이터의
-                  일관성을 유지하면서 사용자가 데이터 삭제의 영향을
+                <span className="block ml-8 mb-4">
+                  데이터의 일관성을 유지하면서 사용자가 데이터 삭제의 영향을
+                  인지하고 결정할 수 있도록 설계했습니다.
                 </span>
-                <span className="block ml-8">
-                  명확히 인지하고 결정할 수 있도록 했습니다.
-                </span>
+                <span className="block ml-8"></span>
               </span>
               <span className="block ml-2 mb-3">
-                <span className="block font-bold">
-                  3. 계층형 데이터 버전 관리 시스템
+                <span className="block font-bold mb-1">
+                  3. 계층형 데이터 버전 관리
                 </span>
                 <span className="block ml-4">
-                  ∙ 계층형 데이터 구조에서 하위 데이터 수정 시 발생하는 데이터
-                  정합성 문제를 해결하기 위해 하위 데이터 수정 시
+                  ∙ 계층형 데이터 구조에서 하위 데이터 수정 시 발생하는{" "}
+                  <strong className="font-bold">데이터 정합성 문제</strong>를
+                  해결하기 위해 하위 데이터 수정 시
                 </span>
                 <span className="block ml-8">
                   상위 데이터의 버전을 관리하여 기존 사용자와 신규 업데이트
@@ -128,9 +136,36 @@ const ClabMVP: React.FC = () => {
                   ∙ 데이터의 버전 관리와 시스템의 확장성을 고려한 아키텍처
                   설계를 진행하며 버전 관리의 중요성,사용자 경험을 고려한
                 </span>
-                <span className="block ml-8">
+                <span className="block ml-8 mb-4">
                   설계의 필요성, 그리고 확장 가능한 아키텍처 설계를
                   경험하였습니다.
+                </span>
+              </span>
+              <span className="block ml-2 mb-5">
+                <span className="block font-bold mb-1">
+                  4. 데이터 후처리 및 분석
+                </span>
+                <span className="block ml-4">
+                  ∙ Naver clova의 STT(Speech to Text) API를 통해 변환된 텍스트
+                  데이터의 오탈자와 발화자 정보를 수정할 수 있는
+                </span>
+                <span className="block ml-8 mb-2">
+                  인터페이스를 구축하였습니다.
+                </span>
+                <span className="block ml-4">
+                  ∙ 데이터 분석을 위해 정제된 텍스트와 메타데이터를 ML서버와
+                  연동하여 자동 문장 분류를 구현하였으며, 분류 결과를
+                </span>
+                <span className="block ml-8 mb-2">
+                  수정할 수 있는 인터페이스를 구축하였습니다.
+                </span>
+                <span className="block ml-4">
+                  ∙ 오디오 데이터를 통해 인사이트를 도출하기 위해 LLM 기반 분석
+                  시스템을 구축하였고, 분석 품질을 지속적으로
+                </span>
+                <span className="block ml-8 mb-2">
+                  개선하기 위해 프롬프트를 수정할 수 있는 인터페이스를
+                  구축하였습니다.
                 </span>
               </span>
             </li>
@@ -162,7 +197,7 @@ const ClabMVP: React.FC = () => {
             </a>
           </div>
         </div>
-        <div className="space-y-10">
+        <div className="space-y-10 flex flex-col justify-center items-center md:h-screen">
           <Image
             src="/clab-mvp.png"
             alt="아키텍쳐"
