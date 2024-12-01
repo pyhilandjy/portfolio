96bb60f2630e1e487d1d6d89dce477ba856363a6
pyhilandjy <hilandjy@gmail.com>
2024-12-01 21:04:34 +0900

    refactor: mvp 커리어 탭으로 이동

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,16 +1,12 @@
 import React from "react";
-import ClabMVP from "./ClabMVP";
 import ConnectsLab from "./ConnectsLab";
 import StockLSTM from "./StockLSTM";
 
 const Projects: React.FC = () => {
   return (
-    <section className="py-20 px-10 bg-gradient-to-b from-blue-300 to-blue-100 text-gray-800">
+    <section className="py-20 px-10 bg-gradient-to-b from-teal-300 to-teal-100 text-gray-800">
       <h1 className="text-5xl font-bold text-center mb-16">∙ Projects ∙</h1>
       <div className="space-y-16">
-        <div>
-          <ClabMVP />
-        </div>
         <div>
           <ConnectsLab />
         </div>
